3f21d192c1d3b39510e194fd825a52ddad2b7a74
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-05-24 20:16:24 -0500

    Add remaining file structure and pseudocode

---
+++ controller/home-routes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+// const {  } = require('../models');
+// const withAuth = require('../utils/auth')
+// TODO: Import the custom middleware
+
+module.exports = router;
